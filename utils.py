ce5b25b9f60154d078ad2860bb270b88c3ae06ca
Owen Brown <owen.brown@gmail.com>
2018-08-07 12:06:04 -0700

    Remove helpers, since format changed.

--- utils.py
+++ utils.py
@@ -1,11 +1,26 @@
 """Optional helper function """
 
 
-def get_station_name(event):
-    """get the station name from the event"""
-    return event['result']['parameters']['origin']['station']
+def get_station_name(event: dict) -> str:
+    """Get the station name that a user spoke from the event
+    
+    Args:
+        event (dict): The payload posted from DialogFlow to our endpoint
+    
+    Returns
+        (str): The station name. This corresponds to the "dialog_flow_entity" key in the stations list
+    """
+    pass
 
 
-def get_direction(event):
-    """get the direction from the event"""
-    return event['result']['parameters']['direction']
+def get_direction(event: dict) -> str:
+    """Get the direction of the train that the user spoke.
+    
+    Args:
+        event (dict): The payload posted from DialogFlow to our endpoint
+    
+    Returns
+        (str): Represents cardinal direction that the user spoke
+    """
+    
+    pass
